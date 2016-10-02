ba0d83e2e494df0231d4dff516d34be080484ea3
Renke Grunwald <renke@renke.org>
2016-10-02 23:08:51 +0200

    v2.0.10

---
+++ lib/index.d.ts
@@ -0,0 +1,19 @@
+import "atom";
+export declare class Plugin {
+    bufferWillSaveDisposables?: any;
+    editorObserverDisposable?: any;
+    config: {
+        sortOnSave: {
+            title: string;
+            description: string;
+            type: string;
+            default: boolean;
+        };
+    };
+    activate(state: any): void;
+    deactivate(): void;
+    private observeEditors();
+    private unobserveEditors();
+    private sortEditor(editor);
+    private sortCurrentEditor();
+}
